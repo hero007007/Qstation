e0390b914c74becca60b2e09900afcbccc4b5ab8
1 <490899446@qq.com>
2016-09-23 09:17:13 +0800

    开始上传

--- Qstation/app/main.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by jlch on 16/9/20.
- */
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
--- Qstation/app/qs.service.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by jlch on 16/9/20.
- */
-import { Injectable }   from '@angular/core';
-
-@Injectable()
-export class QsService{
-
-}
